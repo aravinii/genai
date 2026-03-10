0f8967a3bf4de6a67a3efc4da259f14443cb7d6d
Vinicius.araujo <vinicius.araujo@quintoandar.com.br>
2026-03-10 15:51:30 -0300

    final agent planner

---
+++ real_state_agent/utils/render.py
@@ -0,0 +1,18 @@
+from IPython.display import display, HTML
+import markdown
+
+def render_block(role: str, message: str, color: str):
+    message_ = markdown.markdown(message)
+    html = f"""
+    <div style="
+        background-color: {color};
+        padding: 10px;
+        border-radius: 8px;
+        margin: 5px 0px;
+        font-family: Arial, sans-serif;
+    ">
+        <strong>{role}:</strong><br>
+        {message_.replace('\n', '<br>')}
+    </div>
+    """
+    display(HTML(html))
